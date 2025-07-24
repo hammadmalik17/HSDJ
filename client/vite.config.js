bb4383da1a887ba5bb6b8f3c1400907506be0674
hammadmalik17 <drhammadmalik2020@gmail.com>
2025-07-24 19:09:54 +0530

    Rectify File Structure

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
   },
-  plugins: [],
-}
+})
